e9e518e77836970f5b78f8c8b04b6fd0852111e9
Tea Mitchell <teamitchell@rocketmail.com>
2020-05-05 22:33:18 -0500

    added shots taken increase

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,33 @@
 const teamOneShots = document.querySelector("#teamone-shoot-button")
 const teamTwoShots = document.querySelector("#teamtwo-shoot-button")
 const takenShots = document.querySelector("#teamone-numshots")
+const takenShots2 = document.querySelector("#teamtwo-numshots")
 
 
 teamOneShots.addEventListener("click", function(){
-    console.log("teamOneShots")
-    
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
-      }
+    console.log("Shoot1")
+    let rand = (Math.random() > 0.5)
+    console.log(rand)
 
      let newTakenShots = parseInt(takenShots.innerText) + 1;
 
      takenShots.innerText = newTakenShots
 
+     if(rand){
+         
+
+     }
 
 })
 
 teamTwoShots.addEventListener ("click", function() {
-    var teamTwoShots = random(0,3)
-    var probability = teamTwoShots
+    console.log("Shoot2")
+    function getRandomArbitrary(min, max){
+        return Math.random() * (max - min) +min;
+
+    }
+
+     let newTakenShots2 = parseInt(takenShots2.innerText) + 1;
+
+     takenShots2.innerText = newTakenShots2
 })
